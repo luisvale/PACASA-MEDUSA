6847848ccd993843e2b2f04f3a31df022822b6da
Valerio2385 <luis.valerio@bpc-lat.com>
2024-08-20 16:12:48 -0600

    ss

--- facturacioautomatica/__manifest__.py
+++ facturacioautomatica/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Ajuste PACASA',
+    'name' : 'Ajuste_PACASA',
     'version' : '2.3',
     'author':'s',
     'category': 'Sales',
